dec12dd2b50bb9c058f5965718106ab3f570bb69
munyasya21 <munyasyacynthia@gmail.com>
2023-11-07 15:04:57 +0300

    speciality add update

---
+++ speciality-add.php
@@ -0,0 +1,144 @@
+<?php 
+
+// session_start();
+include('../config.php');
+include('account/config.php');
+include('account/middleware.php');
+include(INCLUDE_PATH . '/logic/account_api.php'); 
+error_reporting(0);
+
+
+if(isset($_POST['submit']))
+{
+$name=$_POST['name'];
+$sql="INSERT INTO  specialities(name) VALUES(:name)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':name',$name,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Speciality Added successfully";
+}
+else
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/doctor-list.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:51 GMT -->
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>BHCS - Speciality List Page</title>
+		
+		<!-- Favicon -->
+        <!-- <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png"> -->
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		
+		<!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		
+		<!-- Feathericon CSS -->
+        <link rel="stylesheet" href="assets/css/feathericon.min.css">
+		
+		<!-- Datatables CSS -->
+		<link rel="stylesheet" href="assets/plugins/datatables/datatables.min.css">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+		
+		<!--[if lt IE 9]>
+			<script src="assets/js/html5shiv.min.js"></script>
+			<script src="assets/js/respond.min.js"></script>
+		<![endif]-->
+    </head>
+    <body>
+	
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+		
+		<?php include('const/header.php') ;
+			  include('const/sidenav.php') ;
+		?>
+			
+
+			
+			<!-- Page Wrapper -->
+            <div class="page-wrapper">
+                <div class="content container-fluid">
+				
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="row">
+							<div class="col-sm-12">
+								<h3 class="page-title">New Speciality</h3>
+								<ul class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+									<li class="breadcrumb-item"><a href="javascript:(0);">Specialities</a></li>
+									<li class="breadcrumb-item active">Speciality</li>
+								</ul>
+								<!-- <a style="float:right;" class="btn btn-success" href="doctor-add.php">Add Patient</a> -->
+								<?php if($error){?><div class="errorWrap" style="background-color: #fa2837; color: #ffffff; margin:10px; padding:10px;"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php }
+				else if($msg){?><div class="succWrap"  style="background-color: #2dcc70; color: #ffffff;margin:10px; padding:10px;"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+							</div>
+						</div>
+					</div>
+					<!-- /Page Header -->
+
+					
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="card">
+								<div class="card-body">
+									<form method="post">
+											<div class="form-group row">
+												<label class="col-form-label col-md-2">Speciality Name</label>
+												<div class="col-md-10">
+													<input type="text" name="name" class="form-control" required>
+												</div>
+											</div>
+											<div class="text-right">
+												<button type="submit" name="submit" class="btn btn-primary">Submit</button>
+											</div>
+									</form>
+								</div>
+							</div>
+						</div>			
+					</div>
+					
+				</div>			
+			</div>
+			<!-- /Page Wrapper -->
+		
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+		
+		<!-- Slimscroll JS -->
+        <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+		
+		<!-- Datatables JS -->
+		<script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
+		<script src="assets/plugins/datatables/datatables.min.js"></script>
+		
+		<!-- Custom JS -->
+		<script  src="assets/js/script.js"></script>
+		
+    </body>
+
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/doctor-list.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:51 GMT -->
+</html>
